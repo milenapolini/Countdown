d4f0914d5b3f103ee55d84fd6bf81567576ab1ef
Milena Polini <milenapolini@MacBook-Air-de-Milena.local>
2023-01-22 13:51:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const birthday = "12 Feb 2023";
+
+function countdown() {
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML= days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    
+
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
+
